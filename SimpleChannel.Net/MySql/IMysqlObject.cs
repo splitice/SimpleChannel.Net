9b8b743c5d9499bc1851f0aa2dbdb7909242b047
splitice <mat999@gmail.com>
2015-12-16 11:59:11 +1100

    MySqlBulkChannel for input and output

---
+++ SimpleChannel.Net/MySql/IMysqlObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SimpleChannel.Net.MySql
+{
+    public interface IMysqlObject
+    {
+        void SetValue(String key, object value);
+        MySqlCommand GetInsertQuery();
+    }
+}
