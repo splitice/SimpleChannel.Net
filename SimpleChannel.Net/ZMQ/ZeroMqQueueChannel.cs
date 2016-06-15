08c68baeb64129ee36a7a851454036aa5a316c6e
splitice <mat999@gmail.com>
2016-06-15 21:02:13 +1000

    Add zeromq channels

---
+++ SimpleChannel.Net/ZMQ/ZeroMqQueueChannel.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Framing.Impl;
+
+namespace SimpleChannel.Net.ZMQ
+{
+    public class ZeroMqQueueChannel<T> : IChannel<T> where T : class
+    {
+        protected readonly String Name;
+        private readonly DataContractSerializer _ser;
+        private PushSocket _publisherSocket;
+        private PullSocket _subscriberSocket;
+        private string _connectionString;
+
+        protected ZeroMqQueueChannel(String name, String connectionString, bool bind)
+        {
+            Name = name;
+            _ser = new DataContractSerializer(typeof(T));
+            String pubStr = connectionString;
+            if (bind)
+            {
+                pubStr = "@" + connectionString;
+            }
+            _publisherSocket = new PushSocket(pubStr);
+            _connectionString = connectionString;
+        }
+
+
+        public bool Offer(T toPut, int ms)
+        {
+            //TODO: just use timeout (but not block)?
+            throw new NotImplementedException();
+        }
+
+        public void Put(T item, String routingKey)
+        {
+            MemoryStream stream = new MemoryStream();
+            _ser.WriteObject(stream, item);
+
+            _publisherSocket.SendMoreFrame(Name).SendFrame(stream.ToArray());
+        }
+
+        /// <summary>
+        /// Put message onto exchange channel
+        /// </summary>
+        /// <param name="item"></param>
+        public void Put(T item)
+        {
+            Put(item, Name);
+        }
+
+        /// <summary>
+        /// Poll for value given timeout
+        /// </summary>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public bool Poll(out T val, int timeout, bool noAck)
+        {
+            if (_subscriberSocket == null)
+            {
+                _subscriberSocket = new PullSocket(">"+_connectionString);
+                _subscriberSocket.Subscribe(Name);
+            }
+
+            //Get the message
+            if (timeout >= 0)
+            {
+                var poll = _subscriberSocket.Poll(PollEvents.PollIn, TimeSpan.FromMilliseconds(timeout));
+                if ((poll & PollEvents.PollIn) != PollEvents.PollIn)
+                {
+                    val = default(T);
+                    return false;
+                }
+            }
+
+            var result = _subscriberSocket.ReceiveFrameBytes();
+            if (result == null)
+            {
+                val = default(T);
+                return false;
+            }
+
+            //Deserialize
+            try
+            {
+                var stream = new MemoryStream(result);
+                val = _ser.ReadObject(stream) as T;
+            }
+            catch (Exception)
+            {
+                val = default(T);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Poll for value given timeout
+        /// </summary>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public bool Poll(out T val, int timeout)
+        {
+            return Poll(out val, timeout, false);
+        }
+
+        /// <summary>
+        /// Blocking consume the next message
+        /// </summary>
+        /// <param name="noAck"></param>
+        /// <returns></returns>
+        public T Take(bool noAck)
+        {
+            T val;
+            Poll(out val, -1, noAck);
+            return val;
+        }
+
+        /// <summary>
+        /// Blocking consume the next message
+        /// </summary>
+        /// <returns></returns>
+        public T Take()
+        {
+            return Take(false);
+        }
+
+        /// <summary>
+        /// Acknowledge the last consumed message
+        /// </summary>
+        public void Ack()
+        {
+        }
+    }
+}
