b06caa870d98497d42cde2fabfc24dc68735dc04
splitice <mat999@gmail.com>
2016-08-14 17:58:17 +1000

    Add /dev/null channel

---
+++ SimpleChannel.Net/Threading/NullChannel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleChannel.Net.Threading
+{
+    public class NullChannel<T>: IChannel<T> where T: class
+    {
+        private bool _producing = true;
+
+        public bool Offer(T toPut, int ms)
+        {
+            return true;
+        }
+
+        public void Put(T item)
+        {
+        }
+
+        public bool Poll(out T val, int timeout)
+        {
+            val = null;
+            return false;
+        }
+
+        public T Take()
+        {
+            throw new NotImplementedException("This method can not be implemented on a Null Channel");
+        }
+
+        public void Ack()
+        {
+            //Do nothing
+        }
+
+        public void CloseProducer()
+        {
+            _producing = false;
+        }
+
+        public bool Producing
+        {
+            get { return _producing; }
+        }
+
+        public void CloseConsumer()
+        {
+        }
+    }
+}
