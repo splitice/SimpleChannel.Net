d7c7080f061afd4a22924929374ca82c08a1eb21
splitice <mat999@gmail.com>
2016-09-06 15:28:17 +1000

    add more tests

--- SimpleChannel.Net.Tests/ZeroMqTests.cs
+++ SimpleChannel.Net.Tests/ZeroMqTests.cs
@@ -18,22 +18,63 @@ namespace SimpleChannel.Net.Tests
     [TestFixture]
     public class ZeroMqTests
     {
-        [TestCase]
-        public void TestZmq()
+        private void SetupConnecitons<T>(out ZeroMqQueueChannel<T> qcOut, out ZeroMqQueueChannel<T> qcIn, int port) where T : class
         {
-            ZeroMqQueueChannel<TestModel> qcOut = new ZeroMqQueueChannel<TestModel>("N", "tcp://127.0.0.1:5557", true);
-            ZeroMqQueueChannel<TestModel> qcIn = new ZeroMqQueueChannel<TestModel>("N", "tcp://127.0.0.1:5557", false);
+            qcOut = new ZeroMqQueueChannel<T>("N", "tcp://127.0.0.1:"+port, true);
+            qcIn = new ZeroMqQueueChannel<T>("N", "tcp://127.0.0.1:" + port, false);
+            
+            //Put into listening mode
+            T tm;
+            qcIn.Poll(out tm, 10);
+        }
+
+        [TestCase]
+        public void TestZmqEmpty()
+        {
+            ZeroMqQueueChannel<TestModel> qcOut, qcIn;
+            SetupConnecitons(out qcOut, out qcIn, 1111);
 
             TestModel tm = null;
             qcIn.Poll(out tm, 10);
             Assert.IsNull(tm);
 
+            qcOut.Dispose();
+            qcIn.Dispose();
+        }
+
+        [TestCase]
+        public void TestZmqPutTake()
+        {
+            ZeroMqQueueChannel<TestModel> qcOut, qcIn;
+            SetupConnecitons(out qcOut, out qcIn, 1112);
+
             TestModel model;
             var task = Task.Run(() => model = qcIn.Take());
 
             qcOut.Put(new TestModel());
-            
+
             Assert.IsTrue(task.Wait(1000));
+
+            qcOut.Dispose();
+            qcIn.Dispose();
+        }
+
+
+        [TestCase]
+        public void TestZmqPutPoll()
+        {
+            ZeroMqQueueChannel<TestModel> qcOut, qcIn;
+            SetupConnecitons(out qcOut, out qcIn, 1113);
+
+            TestModel model;
+
+            qcOut.Put(new TestModel());
+
+            Assert.IsTrue(qcIn.Poll(out model, 1000));
+            Assert.IsNotNull(model);
+
+            qcOut.Dispose();
+            qcIn.Dispose();
         }
     }
 }
